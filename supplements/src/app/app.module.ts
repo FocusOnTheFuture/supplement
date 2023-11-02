9bb26e0d05c9ea8f6529d4453bd38b11f10ab3d5
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-11-02 02:57:00 -0300

    update

--- supplements/src/app/app.module.ts
+++ supplements/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-<<<<<<< HEAD
-import { MenubarComponent } from './pages/menubar/menubar.component';
-=======
+
 import {MenuHamburguerComponent} from './pages/menubar/menu-hamburguer/menu-hamburguer.component'
 import { MenubarComponent } from './pages/menubar/menubar.component'
->>>>>>> 692108c018f607913440d4505ad68f18b81a6d65
+
 import { HomeComponent } from './pages/home/home.component';
 
 import { DefaultButtonComponent } from './pages/default-button/default-button.component';
