e5b285c359c835312ff5d89e877d6ca5a47c22b9
FocusOnTheFuture <87251849+EstevanSantiagoDutra@users.noreply.github.com>
2023-11-02 01:55:39 -0300

    up

---
+++ supplements/src/app/pages/menubar/menu-hamburguer/menu-hamburguer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuHamburguerComponent } from './menu-hamburguer.component';
+
+
+
+@NgModule({
+  declarations: [
+    MenuHamburguerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MenuHamburguerModule { }
