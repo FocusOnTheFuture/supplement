3d0678c3f9e28c4714e620bf2fd2ebcedf80ccd8
FocusOnTheFuture <87251849+EstevanSantiagoDutra@users.noreply.github.com>
2023-11-02 02:35:39 -0300

    update.menuhamburguer

--- supplements/src/app/app.module.ts
+++ supplements/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MenuHamburguerComponent} from './pages/menubar/menu-hamburguer/menu-hamburguer.component'
 import { MenubarComponent } from './pages/menubar/menubar.component'
 import { HomeComponent } from './pages/home/home.component';
 
@@ -13,6 +14,7 @@ import { DefaultButtonComponent } from './pages/default-button/default-button.co
 @NgModule({
   declarations: [
     AppComponent,
+    MenuHamburguerComponent,
     MenubarComponent,
     HomeComponent,
     DefaultButtonComponent
--- supplements/src/app/pages/menubar/menu-hamburguer/menu-hamburguer.component.ts
+++ supplements/src/app/pages/menubar/menu-hamburguer/menu-hamburguer.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-hamburguer.component.css']
 })
 export class MenuHamburguerComponent {
+  isHidden: boolean = true;
 
+  toggleList() {
+    this.isHidden = !this.isHidden;
+  }
 }
