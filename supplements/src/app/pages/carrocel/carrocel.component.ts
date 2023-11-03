f12fcf63b7b6da3c5df8323132b68e7cea37f67f
FocusOnTheFuture <87251849+EstevanSantiagoDutra@users.noreply.github.com>
2023-11-02 23:49:23 -0300

    carrocel

--- supplements/src/app/pages/carrocel/carrocel.component.ts
+++ supplements/src/app/pages/carrocel/carrocel.component.ts
@@ -6,24 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrocel.component.css']
 })
 export class CarrocelComponent {
+  slideIndex = 0;
 
-  currentSlide = 0;
-
-  goToSlide(n: number) {
-    this.currentSlide = n;
-    let carousel = document.querySelector('.carousel-inner');
-    if (carousel) {
-      carousel.style.transform = 'translateX(-' + (this.currentSlide * 100) + '%)';
+  showSlides() {
+    const slides = document.getElementsByClassName("carousel-item") as HTMLCollectionOf<HTMLElement>;
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-
-    let buttons = document.querySelectorAll('.carousel-indicators button');
-    buttons.forEach((button, index) => {
-      if (index === n) {
-        button.classList.add('active');
-      } else {
-        button.classList.remove('active');
-      }
-    });
+    slides[this.slideIndex].style.display = "block";
   }
 
+  prevSlide() {
+    const slides = document.getElementsByClassName("carousel-item") as HTMLCollectionOf<HTMLElement>;
+    this.slideIndex = (this.slideIndex - 1 + slides.length) % slides.length;
+    this.showSlides();
+  }
+
+  nextSlide() {
+    const slides = document.getElementsByClassName("carousel-item") as HTMLCollectionOf<HTMLElement>;
+    this.slideIndex = (this.slideIndex + 1) % slides.length;
+    this.showSlides();
+  }
+
+  ngAfterViewInit() {
+    this.showSlides();
+  }
 }
--- supplements/src/app/pages/carrocel/carrocel.module.ts
+++ supplements/src/app/pages/carrocel/carrocel.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarrocelComponent } from './carrocel.component';
-
+import {BoxItemPriceComponent} from '../box-item-price/box-item-price.component';
 
 
 @NgModule({
   declarations: [
-    CarrocelComponent
+    CarrocelComponent,
+    BoxItemPriceComponent
   ],
   imports: [
     CommonModule
