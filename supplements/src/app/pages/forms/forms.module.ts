24671bb14616f2e0b508cf9d342b5fc4a672d455
FocusOnTheFuture <87251849+EstevanSantiagoDutra@users.noreply.github.com>
2023-11-05 21:32:09 -0300

    update for forms

---
+++ supplements/src/app/pages/forms/forms.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsComponent } from './forms.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormsComponent,
+    CheckoutComponent,
+    AccountComponent,
+    LoginComponent,
+    CreateaccountComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormsModule { }
