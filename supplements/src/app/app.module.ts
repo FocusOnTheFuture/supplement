4f73886960725db43c47a9ba07e79d7df1b9c6e6
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-11-05 21:15:50 -0300

    import menubar module

--- supplements/src/app/app.module.ts
+++ supplements/src/app/app.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeModule } from './pages/home/home.module'
 import { FooterModule } from './pages/footer/footer.module';
+import { MenubarModule } from './pages/menubar/menubar.module';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { MenusearchComponent} from './pages/menubar/menusearch/menusearch.component'
-import { MenuHorizontalComponent } from './pages/menubar/menu-horizontal/menu-horizontal.component';
-import {MenuHamburguerComponent} from './pages/menubar/menu-hamburguer/menu-hamburguer.component'
-import { MenubarComponent } from './pages/menubar/menubar.component'
 
 
 import { DefaultButtonComponent } from './pages/default-button/default-button.component';
@@ -18,7 +14,6 @@ import { DefaultButtonComponent } from './pages/default-button/default-button.co
 
 import { BoxItemPriceComponent } from './pages/box-item-price/box-item-price.component';
 import { BoxTotalPriceComponent } from './pages/box-total-price/box-total-price.component';
-
 
 
 import { FullServicesComponent } from './pages/full-services/full-services.component';
@@ -31,10 +26,6 @@ import {CarrocelComponent} from './pages/carrocel/carrocel.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenusearchComponent,
-    MenuHorizontalComponent,
-    MenuHamburguerComponent,
-    MenubarComponent,
     DefaultButtonComponent,
     BoxItemPriceComponent,
     BoxTotalPriceComponent,
@@ -45,6 +36,7 @@ import {CarrocelComponent} from './pages/carrocel/carrocel.component';
     BrowserModule,
     AppRoutingModule,
     HomeModule,  
+    MenubarModule,
     FooterModule
   ],
   providers: [],
--- supplements/src/app/pages/menubar/menubar.module.ts
+++ supplements/src/app/pages/menubar/menubar.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenubarComponent } from './menubar.component';
-
+import { MenuHamburguerComponent } from './menu-hamburguer/menu-hamburguer.component';
+import { MenuHorizontalComponent } from './menu-horizontal/menu-horizontal.component';
+import { MenusearchComponent } from './menusearch/menusearch.component';
 
 
 @NgModule({
   declarations: [
-    MenubarComponent
+    MenubarComponent,
+    MenuHamburguerComponent,
+    MenuHorizontalComponent,
+    MenusearchComponent
   ],
   imports: [
     CommonModule
-  ], 
-  exports: [ 
+  ],
+  exports: [
     MenubarComponent
   ]
 })
