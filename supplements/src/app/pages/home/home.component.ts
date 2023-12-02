29f19763268b9e51fc41f9efa67f3ba155650fa9
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-12-02 01:24:24 -0300

    button dynamic @Input

--- supplements/src/app/pages/home/home.component.ts
+++ supplements/src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { DefaultButtonComponent } from '../default-button/default-button.component'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  buttonLabel: string = 'Ver todos os produtos';
+  buttonTest: string = 'Teste';
 }
+
+
