cbdb7642732ae96f530a344ad9eec4830af8d713
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-12-04 13:47:11 -0300

    button homepage page home

--- supplements/src/app/pages/home/home.component.ts
+++ supplements/src/app/pages/home/home.component.ts
@@ -9,8 +9,7 @@ import { Router } from '@angular/router';
 })
 
 export class HomeComponent {
-  buttonLabel: string = 'Ver todos os produtos';
-  buttonTest: string = 'Teste';
+  allProducts: string = 'Ver todos os produtos';
 
   constructor(private router: Router) { }
 
