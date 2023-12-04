79816b0a6126f93fc62399c18b2dddc5e4676a2c
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-12-04 13:53:23 -0300

    button homepage page wishlist

--- supplements/src/app/pages/box-total-price/box-total-price.component.ts
+++ supplements/src/app/pages/box-total-price/box-total-price.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-box-total-price',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./box-total-price.component.css']
 })
 export class BoxTotalPriceComponent {
+  Cart: string = 'Ir para carrinho';
 
+  constructor(private router: Router) { }
+
+  navegarParaOutraPagina() {
+    this.router.navigate(['/']);
+  }
 }
