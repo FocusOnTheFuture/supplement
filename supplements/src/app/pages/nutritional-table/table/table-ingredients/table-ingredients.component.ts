9840f000f48564f70ff10982fa0b4e341f99bd36
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-12-04 19:51:45 -0300

    add labels

--- supplements/src/app/pages/nutritional-table/table/table-ingredients/table-ingredients.component.ts
+++ supplements/src/app/pages/nutritional-table/table/table-ingredients/table-ingredients.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-ingredients',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-ingredients.component.css']
 })
 export class TableIngredientsComponent {
+  @Input()
+  labelTitle:string = '';
+
+  @Input()
+  labelDescription:string = '';
+
+  @Input()
+  labelAttention:string = '' ;
 
 }
--- supplements/src/app/pages/nutritional-table/table/table-values/kcal/kcal.component.ts
+++ supplements/src/app/pages/nutritional-table/table/table-values/kcal/kcal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kcal',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./kcal.component.css']
 })
 export class KcalComponent {
+  @Input()
+  labelTitle = '';
+  
+  @Input()
+  labelValue = '';
 
+  @Input()
+  labelVD = '';
 }
