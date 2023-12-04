d7569f83e567118442dcc4031ab6b5233329ca87
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-12-04 14:06:33 -0300

    button homepage page pagelogin

--- supplements/src/app/pages/forms/login/login.component.ts
+++ supplements/src/app/pages/forms/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  Login: string = 'Log in';
 
+  constructor(private router: Router) { }
+
+  navegarParaOutraPagina() {
+    this.router.navigate(['/login']);
+  }
 }
