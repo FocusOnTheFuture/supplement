71bd9284804bb21eb9c3af6228e5317c51937fba
FocusOnTheFuture <focus.otfuture@gmail.com>
2023-12-11 21:11:00 -0300

    wishlist plate

--- supplements/src/app/pages/page-wish-list/page-wish-list.component.ts
+++ supplements/src/app/pages/page-wish-list/page-wish-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-page-wish-list',
   templateUrl: './page-wish-list.component.html',
   styleUrls: ['./page-wish-list.component.css']
 })
-export class PageWishListComponent {
 
+
+export class PageWishListComponent {
+  imgCover:string = "../../../assets/Suplemento box category.png";
 }
